d2088291a8fecfbf62c01794929724374d538796
PepICS <jgomez.alvarez.hj23.ics@gencat.cat>
2025-02-19 16:18:12 +0100

    all packages working in R

--- rstudio/install.R
+++ rstudio/install.R
@@ -3,10 +3,12 @@ install.packages("BiocManager", repos = "http://cran.r-project.org")
 # Instal·lar GEOquery i totes les seves dependències automàticament
 BiocManager::install("GEOquery", ask=FALSE, update=TRUE, dependencies=TRUE, lib="/usr/local/lib/R/site-library")
 
+# Instal·lar altres paquets necessaris
+BiocManager::install(c("limma", "edgeR", "bsseq", "pheatmap"), ask=FALSE, update=TRUE, dependencies=TRUE, lib="/usr/local/lib/R/site-library")
+
 # Comprovar si està instal·lat correctament
 if (!requireNamespace("GEOquery", quietly = TRUE)) {
     stop("❌ ERROR: GEOquery no s'ha pogut instal·lar correctament.")
 } else {
     print("✅ GEOquery s'ha instal·lat correctament!")
 }
-
